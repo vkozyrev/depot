1c3b5b7ceb1fae9a7f952c102160ed1ac66c0fd5
Vladimir Kozyrev <kozyrevvladimir@gmail.com>
2015-08-13 17:24:46 -0700

    - Implemented an admin system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+
   def new
   end
 
@@ -6,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      redirect_to admin_index_path
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
 end
